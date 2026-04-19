c2095e4aaeaa3849e4080696e5b008814b8d5539
Kanishk Mankar <kanishkmankar77@gmail.com>
2026-04-20 01:20:37 +0530

    fix the script

--- poc/gcs_testing/queries/GQ05_date_range/duckdb.sql
+++ poc/gcs_testing/queries/GQ05_date_range/duckdb.sql
@@ -13,31 +13,7 @@ SELECT
 FROM read_csv_auto(
     's3://<GCS_GLUSR_PREMIUM_LISTING_PREFIX>',
     header = true,
-    null_padding = true,
-    columns = {
-        'glusr_premium_listing_id': 'BIGINT',
-        'fk_glusr_usr_id': 'BIGINT',
-        'glusr_premium_mcat_id': 'BIGINT',
-        'glusr_premium_city_id': 'BIGINT',
-        'flag_premium_listing': 'VARCHAR',
-        'fk_service_id': 'BIGINT',
-        'fk_cust_to_serv_id': 'BIGINT',
-        'pl_kwrd_term_upper': 'VARCHAR',
-        'glusr_premium_enable': 'VARCHAR',
-        'glusr_premium_added_date': 'TIMESTAMP',
-        'last_modified_date': 'TIMESTAMP',
-        'glusr_premium_updatedby_id': 'BIGINT',
-        'glusr_premium_updatedby': 'VARCHAR',
-        'glusr_premium_updatescreen': 'VARCHAR',
-        'glusr_premium_ip': 'VARCHAR',
-        'glusr_premium_ip_country': 'VARCHAR',
-        'glusr_premium_hist_comments': 'VARCHAR',
-        'glusr_premium_updatedby_url': 'VARCHAR',
-        'category_type': 'VARCHAR',
-        'location_type': 'VARCHAR',
-        'location_iso': 'VARCHAR',
-        'category_location_credit_value': 'DOUBLE'
-    }
+    null_padding = true
 )
 WHERE glusr_premium_added_date BETWEEN TIMESTAMP '2024-01-01 00:00:00' AND TIMESTAMP '2024-03-31 23:59:59'
 GROUP BY 1, 2
