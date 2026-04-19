ca05b4930fbc9f89dda3b534b7fb70e0db2925e8
Kanishk Mankar <kanishkmankar77@gmail.com>
2026-04-20 02:33:56 +0530

    fix the script

--- poc/gcs_testing/queries/GQ09_window_func/doris.sql
+++ poc/gcs_testing/queries/GQ09_window_func/doris.sql
@@ -16,6 +16,7 @@ WITH user_category_agg AS (
         "s3.region"        = "<GCS_REGION>",
         "s3.access_key"    = "<GCS_HMAC_ACCESS_KEY>",
         "s3.secret_key"    = "<GCS_HMAC_SECRET>",
+        "use_path_style"   = "true",
         "format"           = "csv",
         "column_separator" = ",",
         "columns"          = "glusr_premium_listing_id BIGINT, fk_glusr_usr_id BIGINT, glusr_premium_mcat_id BIGINT, glusr_premium_city_id BIGINT, flag_premium_listing VARCHAR(10), fk_service_id BIGINT, fk_cust_to_serv_id BIGINT, pl_kwrd_term_upper VARCHAR(500), glusr_premium_enable VARCHAR(10), glusr_premium_added_date DATETIME, last_modified_date DATETIME, glusr_premium_updatedby_id BIGINT, glusr_premium_updatedby VARCHAR(255), glusr_premium_updatescreen VARCHAR(255), glusr_premium_ip VARCHAR(100), glusr_premium_ip_country VARCHAR(40), glusr_premium_hist_comments VARCHAR(1000), glusr_premium_updatedby_url VARCHAR(255), category_type VARCHAR(50), location_type VARCHAR(50), location_iso VARCHAR(10), category_location_credit_value DOUBLE"
